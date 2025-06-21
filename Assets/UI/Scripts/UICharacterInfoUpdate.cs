a3afb3e73ca27387cebe38ecd24ea3ce466b8369
AlexUji <90849122+AlexUji@users.noreply.github.com>
2025-06-21 10:33:10 +0200

    UI improvement
    La UI muestra el nivel del personaje

--- Assets/UI/Scripts/UICharacterInfoUpdate.cs
+++ Assets/UI/Scripts/UICharacterInfoUpdate.cs
@@ -11,10 +11,13 @@ public class UICharacterInfoUpdate : MonoBehaviour
         
         transform.GetChild(0).GetComponent<Image>().sprite = targetedCharacter.portrait;
         transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = targetedCharacter.characterName;
-
+        if (targetedCharacter.level<10)       
+            transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = "0"+targetedCharacter.level;
+        else
+            transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = targetedCharacter.level.ToString();
 
         //HP
-        var hpinfo = transform.GetChild(2);
+        var hpinfo = transform.GetChild(4);
         hpinfo.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = targetedCharacter.currentHP.ToString();
         hpinfo.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = "/" + targetedCharacter.MaxHP.ToString();
 
@@ -24,7 +27,7 @@ public class UICharacterInfoUpdate : MonoBehaviour
 
 
         //MP
-        var mpinfo = transform.GetChild(3);
+        var mpinfo = transform.GetChild(5);
         mpinfo.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = targetedCharacter.currentMP.ToString();
         mpinfo.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = "/" + targetedCharacter.MaxMP.ToString();
 
